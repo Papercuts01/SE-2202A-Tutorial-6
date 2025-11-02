68752128133c7ff46e3f2ea391d8d7f0070cbc89
Papercuts01 <111815913+Papercuts01@users.noreply.github.com>
2025-11-01 22:27:04 -0400

    Add files via upload

---
+++ pointConstructorSolution.js
@@ -0,0 +1,47 @@
+// Create a template object (prototype) for a 2D point
+let PointPrototype = {
+
+    x: 0,
+    y: 0,
+
+    print: function() {
+        console.log("(" + this.x + ", " + this.y + ")");
+    }
+};
+
+// Function that creates a point object using the prototype
+function createPoint(x, y) {
+    
+    let p = Object.create(PointPrototype);
+    p.x = x;
+    p.y = y;
+    return p;
+}
+
+// test the prototype-based objects
+// do not change
+let p1 = createPoint(3, 4);
+p1.print();
+p1 = createPoint(10, 15);
+p1.print();
+p1 = createPoint(-2, 8);
+p1.print();
+
+// Now create a constructor function version
+function Point(x, y) {
+    
+    this.x = x;
+    this.y = y;
+
+    this.print = function() {
+       console.log("(" + this.x + ", " + this.y + ")");
+    };
+}
+
+
+let p2 = new Point(3, 4);
+p2.print();
+p2 = //make with constructor function
+p2.print();
+p2 = //make with constructor function
+p2.print();
---
+++ pointFunctionSolution.js
@@ -0,0 +1,39 @@
+function Point(x, y) {
+
+    this.x = x;
+    this.y = y;
+
+    this.midpoint = function(p2){
+        console.log("(" + this.x + ", " + this.y + ")");
+    }
+
+    this.midpoint = function(p2){
+        // midpoint between (this.x, this.y) and (p2.x, p2.y)
+        // correct formula:
+        // x = (x1 + x2) / 2
+        // y = (y1 + y2) / 2
+        let mx = (this.x + p2.x) / 2;
+        let my = (this.y + p2.y) / 2;
+        return new Point(mx, my);
+    }
+
+
+    this.scaleTo = function(p2,t){
+        let sx = this.x + (p2.x - this.x) * s;
+        let sy = this.y + (p2.y - this.y) * s;
+        return new Point(sx, sy);
+    }
+}
+
+
+/* Do not change the lines below */
+let p1 = new Point(1,5)
+let p2 = new Point(5,7)
+p1.midpoint(p2).print()//expect (3, 6)
+p2.midpoint(p1).print()//expect (3, 6)
+
+/* Do not change the lines below */
+let p3 = new Point(0,0)
+let p4 = new Point(4,8)
+p3.scaleTo(p4,0.25).print(1,2)// expect (1, 2)
+p4.scaleTo(p3, 0.75).print() // expect (1, 2)
